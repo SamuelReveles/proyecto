be323bbcafe391bf49a67babdc0f7450aa33f98f
SamuelReveles <91803174+SamuelReveles@users.noreply.github.com>
2022-07-28 02:25:43 -0500

    Sockets

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -15,9 +15,10 @@ const usuarios = new Usuarios();
 const socketController = (socket) => {
 
     //Conexión, agregar un usuario a la lista de usuarios conectados
-    const { id } = jwt.verify(payload.jwt, process.env.SIGNJWT);
-    console.log('Conectado: ' + id);
-    usuarios.agregarUsuario(socket.id, id);
+    socket.on('conectar', (payload) => {
+        const { id } = jwt.verify(payload.jwt, process.env.SIGNJWT);
+        usuarios.agregarUsuario(socket.id, id);
+    });
 
     //Eliminar de la lista y cambiar la última conexión
     socket.on('disconnect', async (payload) => {
